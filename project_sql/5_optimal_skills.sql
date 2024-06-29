c93728abd0c24fb839bbfc0e7093df3c51f30aea
Hammad <hammadaslam382@gmail.com>
2024-06-29 18:12:07 +0500

    optimal skills

---
+++ project_sql/5_optimal_skills.sql
@@ -0,0 +1,86 @@
+/*
+ Answer: What are the most optimal skills to learn (aka it’s in high demand and a high-paying skill)?
+ - Identify skills in high demand and associated with high average salaries for Data Analyst roles
+ - Concentrates on remote positions with specified salaries
+ - Why? Targets skills that offer job security (high demand) and financial benefits (high salaries), 
+ offering strategic insights for career development in data analysis
+ */
+-- Identifies skills in high demand for Machine Learning Engineer roles
+-- Use Query #3
+WITH skills_demand AS (
+    SELECT skills_dim.skill_id,
+        skills_dim.skills,
+        COUNT(skills_job_dim.job_id) AS demand_count
+    FROM job_postings_fact
+        INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+        INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE job_title_short = 'Machine Learning Engineer'
+        AND salary_year_avg IS NOT NULL
+        AND job_work_from_home = True
+    GROUP BY skills_dim.skill_id
+),
+-- Skills with high average salaries for Machine Learning Engineer roles
+-- Use Query #4
+average_salary AS (
+    SELECT skills_job_dim.skill_id,
+        ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_salary
+    FROM job_postings_fact
+        INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+        INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE job_title_short = 'Machine Learning Engineer'
+        AND salary_year_avg IS NOT NULL
+        AND job_work_from_home = True
+    GROUP BY skills_job_dim.skill_id
+) -- Return high demand and high salaries for 10 skills 
+SELECT skills_demand.skill_id,
+    skills_demand.skills,
+    demand_count,
+    avg_salary
+FROM skills_demand
+    INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
+WHERE demand_count > 10
+ORDER BY avg_salary DESC,
+    demand_count DESC
+LIMIT 25;
+-- rewriting this same query more concisely
+SELECT skills_dim.skill_id,
+    skills_dim.skills,
+    COUNT(skills_job_dim.job_id) AS demand_count,
+    ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_salary
+FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE job_title_short = 'Machine Learning Engineer'
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = True
+GROUP BY skills_dim.skill_id
+HAVING COUNT(skills_job_dim.job_id) > 10
+ORDER BY avg_salary DESC,
+    demand_count DESC
+LIMIT 25;
+Result: -- [
+--   {
+--     "skill_id": 76,
+--     "skills": "aws",
+--     "demand_count": "12",
+--     "avg_salary": "168402"
+--   },
+--   {
+--     "skill_id": 0,
+--     "skills": "sql",
+--     "demand_count": "11",
+--     "avg_salary": "152983"
+--   },
+--   {
+--     "skill_id": 1,
+--     "skills": "python",
+--     "demand_count": "33",
+--     "avg_salary": "150570"
+--   },
+--   {
+--     "skill_id": 101,
+--     "skills": "pytorch",
+--     "demand_count": "11",
+--     "avg_salary": "122893"
+--   }
+-- ]
